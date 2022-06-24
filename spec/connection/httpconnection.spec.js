24b7e14ca1cf3fce76378503527aba15808c8c4c
apptesting <marchuanv@sahomeloans.com>
2022-06-24 15:50:06 +0200

    test test test

--- spec/connection/httpconnection.spec.js
+++ spec/connection/httpconnection.spec.js
@@ -1,32 +1,27 @@
 const { HttpConnection } = require("../../lib/http/httpconnection");
 
-describe("when opeing an http connection given a hostname and port number", function() {
- it("it should listen for incoming connections", function() {
-  
-    // Arrange
-  const connection = new HttpConnection();
-
-  // Act
-  connection.open({ host: 'localhost', port: 3000 });
-
-  // Assert
-  expect(connection.isOpen()).toBeTruthy();
-  connection.close();
- });
-});
-
-describe("when getting host information from an open http connection", function() {
-    it("it should return the server host address", function() {
-     
+describe("when opening an http connection given a hostname and port number", function() {
+    beforeAll(() => {
+        this.connection = new HttpConnection();
+    });
+    it("it should have an open connection", async () => {
         // Arrange
-        const connection = new HttpConnection();
-        connection.open({ host: 'localhost', port: 3000 });
 
         // Act
-        const address = connection.getHostInfo();
+        await this.connection.open({ host: 'localhost', port: 3000 });
+
+        // Assert
+        expect(this.connection.isOpen()).toBeTruthy();
+    });
+    it("it should return the server host address", async () => {
+     
+        // Arrange
+        expect(this.connection.isOpen()).toBeTruthy();
+
+        // Act
+        const address = this.connection.getServerAddress();
    
         // Assert
         expect(address).not.toBeNull();
-        connection.close();
     });
-   });
+});
