ef121b889ee4b568179f556da95648f303e5895c
apptesting <marchuanv@sahomeloans.com>
2022-06-28 00:28:30 +0200

    test test test

--- spec/messagebus/messagebus.spec.js
+++ spec/messagebus/messagebus.spec.js
@@ -5,10 +5,11 @@ describe("when publishing a message", function() {
   // Arrange
   let assertCallback;
   let callback = ({ from, data }) => assertCallback({ from, data });
+  const address = { address: 'localhost', port: 3000 };
   const channelName = 'messagebustest';
   const expectedData = 'hello from messagebus test';
   const messageBusFactory = new MessageBusFactory();
-  const messageBus = messageBusFactory.createunsecure();
+  const messageBus = messageBusFactory.createunsecure({ address });
   messageBus.subscribe({ channelName, callback });
   //{ host: 'localhost', port: 3000 }
   // Act
