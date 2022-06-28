31f55f633bd6b913c4b7b84977b0f00fcc06251b
apptesting <marchuanv@sahomeloans.com>
2022-06-28 16:17:29 +0200

    test test test

--- spec/messagebus/messagebus.spec.js
+++ spec/messagebus/messagebus.spec.js
@@ -1,29 +1,30 @@
 const { MessageBusFactory } = require('../../lib/messagebusfactory');
 describe("when publishing a message", function() {
  it("it should send the same message to all subscribers", function(done) {
-  
-  // Arrange
-  let assertCallback;
-  let callback = ({ from, data }) => assertCallback({ from, data });
-  const timeout = 5000;
-  const hostAddress = { address: 'localhost', port: 3000 };
-  const channelName = 'messagebustest';
-  const expectedData = 'hello from messagebus test';
-  const messageBusFactory = new MessageBusFactory({ hostAddress, timeout });
-  const messageBus = messageBusFactory.createunsecure();
-  messageBus.subscribe({ channelName, callback });
-  //{ host: 'localhost', port: 3000 }
-  // Act
-  setTimeout( async () => {
-    await messageBus.publish({ channelName, data: expectedData });
-  },1000);
 
-  // Assert
-  assertCallback = ({ from, data }) => {
-    expect(from).toEqual('localhost:3000');
-    expect(data).toEqual(expectedData);
-    done();
-  };
+    // Arrange
+    let assertCallback;
+    let callback = ({ from, data }) => assertCallback({ from, data });
+    const timeout = 5000;
+    const hostAddress = { address: 'localhost', port: 3000 };
+    const channelName = 'messagebustest';
+    const expectedData = 'hello from messagebus test';
+    const messageBusFactory = new MessageBusFactory({ hostAddress, timeout });
+    const messageBus = messageBusFactory.createunsecure();
+    messageBus.subscribe({ channelName, callback });
+    //{ host: 'localhost', port: 3000 }
 
- })
+    // Act
+    setTimeout( async () => {
+      await messageBus.publish({ channelName, data: expectedData });
+    },1000);
+
+    // Assert
+    assertCallback = ({ from, data }) => {
+      expect(from).toEqual('localhost:3000');
+      expect(data).toEqual(expectedData);
+      done();
+    };
+    
+  })
 });
