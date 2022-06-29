eaa3051b4baeafe9236fde8b894b487dca049916
apptesting <marchuanv@sahomeloans.com>
2022-06-29 15:17:57 +0200

    default config

--- spec/messagehandler/httpmessagehandler.spec.js
+++ spec/messagehandler/httpmessagehandler.spec.js
@@ -1,36 +1,34 @@
-const { HttpRequestMessage } = require("../../lib/http/httprequestmessage");
 const { MessageStatus } = require("../../lib/messagestatus");
 const { HttpMessageHandlerFactory } = require("../../lib/http/httpmessagehandlerfactory");
 const { HttpMessageFactory } = require('../../lib/http/httpmessagefactory');
 const { MessageFactory } = require("../../lib/messagefactory");
 
-xdescribe("when asking the http message handler to send and receive an http request messages", function() {
+describe("when asking the http message handler to send and receive an http request messages", function() {
   it("it should succeed without any errors", async function() {
     
     // Arrange
     const hostAddress = { address: 'localhost', port: 3000 };
-    const recipientAddress = { address: 'localhost', port: 3000 };
+    const sender = { address: 'localhost', port: 3000 };
     const httpMessageHandlerFactory = new HttpMessageHandlerFactory({ hostAddress, timeout: 5000 });
     const httpMessageHandler = httpMessageHandlerFactory.createunsecure();
     const messageFactory = new MessageFactory();
     const httpMessageFactory = new HttpMessageFactory({ messageFactory });
 
     httpMessageHandler.receive({ callback: ({ httpRequestMessage }) => {
-      if (!(httpRequestMessage instanceof HttpRequestMessage)) {
-        throw new Error("the 'httpRequestMessage' parameter is null, undefined or not of type: HttpRequestMessage");
-      }
-      return httpMessageFactory.createHttpResponseMessage({ 
+      expect(httpRequestMessage).not.toBeNull();
+      const httpResponseMessage = httpMessageFactory.createHttpResponseMessage({ 
         data: 'Hello From Server!',
         headers: {},
         messageStatus: new MessageStatus({ code: 0 })
       });
+      return { httpResponseMessage };
     }});
 
     // Act
-    const responseMessage = await httpMessageHandler.send({ recipientAddress, data: 'Hello World!' });
+    const { httpResponseMessage } = await httpMessageHandler.send({ path: '/', headers: { sender }, method: 'POST', data: 'Hello World!' });
 
     // Assert
-    expect(responseMessage).not.toBeNull();
-    expect(responseMessage.getContent()).toEqual('Hello From Server!')
+    expect(httpResponseMessage).not.toBeNull();
+    expect(httpResponseMessage.getContent()).toEqual('Hello From Server!')
   });
 });
