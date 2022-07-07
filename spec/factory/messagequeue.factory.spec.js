90bd601a6c13f5a5302ebf838367b8478a0ded70
apptesting <marchuanv@sahomeloans.com>
2022-07-07 14:39:20 +0200

    refactor

---
+++ spec/factory/messagequeue.factory.spec.js
@@ -0,0 +1,12 @@
+const { createMessageQueue } = require('C:\\component\\lib\\factory\\messagequeue.factory.js');
+describe('when asking the MessageQueue factory to create an instance', function() {
+  it("it should succeed without any errors", function() {
+    // Arrange
+    const {} = require('C:\\component\\spec\\factory\\messagequeue.factory.spec.variables.json');
+
+    // Act
+    const instance = createMessageQueue({});
+    // Assert
+    expect(instance).not.toBeNull();
+  });
+});
