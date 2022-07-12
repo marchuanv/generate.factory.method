394b52fc1bf532360a0abd46f262fb1153045858
apptesting <marchuanv@sahomeloans.com>
2022-07-12 23:52:44 +0200

    refactor

--- spec/messagebus/subscription.spec.js
+++ spec/messagebus/subscription.spec.js
@@ -1,12 +1,22 @@
-const { Subscription } = require('../../lib/factory/subscription.factory');
 describe("when receiving data", function() {
  it("it should notify all subscribers", function() {
   
-  // Arrange
+    // Arrange
+    const channelName = 'channeltest';
+    const expectedData = 'subscriptioncallback';
+    const host = 'localhost';
+    const port = 3000;
+    const { createSubscription } = require('../../lib/factory/subscription.factory');
+    const { subscription } = createSubscription({ channelName });
+    subscription.onDataReceived({ callback: ({ host, port, data }) => {
+        // Assert
+        expect(host).toEqual('localhost');
+        expect(port).toEqual(3000);
+        expect(data).toEqual(expectedData);
+    }});
 
-  // Act
-
-  // Assert
+    // Act
+    subscription.receiveData({ host, port, data: expectedData  });
 
  })
 });
