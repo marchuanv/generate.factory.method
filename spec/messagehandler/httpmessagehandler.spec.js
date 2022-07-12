992bd8de972668c2d122685885a783164abba380
apptesting <marchuanv@sahomeloans.com>
2022-07-13 00:38:21 +0200

    refactor

--- spec/messagehandler/httpmessagehandler.spec.js
+++ spec/messagehandler/httpmessagehandler.spec.js
@@ -1,35 +1,35 @@
-describe("when asking the http message handler to send and receive an http request messages", function() {
+describe("when asking the http message handler to send, receive and respond, to a request messages", function() {
 
-  it("it should succeed without any errors", async () => {
+  it("it should succeed without any errors", async (done) => {
     
     // Arrange
     const userId = 'joe';
     const sender = { host: 'localhost', port: 2000 };
-    const epxectedData = 'Hello World From Server';
+    const expectedData = 'Hello World From Server';
     const { createMessage } = require('../../lib/factory/message.factory');
     const { createHttpRequestMessage } = require('../../lib/factory/httprequestmessage.factory');
     const { createHttpMessageHandler } = require('../../lib/factory/httpmessagehandler.factory');
-    const { messageQueue, httpMessageHandler } = createHttpMessageHandler({ userId });
+    const { httpMessageHandler } = createHttpMessageHandler({ userId });
 
-    httpMessageHandler.receive({ callback: ({ httpRequestMessage }) => {
-      expect(httpRequestMessage).not.toBeNull();
-      const { message } = createMessage({ userId, data: epxectedData, metadata: { sender }, messageStatusCode: 200 });
-      return { responseMessage: message };
+    httpMessageHandler.receive({ callback: async ({ requestMessage }) => {
+      expect(requestMessage).not.toBeNull();
+
+      const data = requestMessage.getContent();
+      const { host, port } = requestMessage.getSenderAddress();
+
+      expect(host).toEqual('localhost');
+      expect(port).toEqual(2000);
+      expect(data).toEqual(expectedData);
+
+      const { message } = createMessage({ userId, data: expectedData, metadata: { sender }, messageStatusCode: 200 });
+      await httpMessageHandler.respond({ responseMessage: message });
+
+      done();
     }});
 
-    // //emulate server
-    // const { httpRequestMessage } =  createHttpRequestMessage({ 
-    //                                     method: 'POST',
-    //                                     userId,
-    //                                     data: 'Hello World!',
-    //                                     metadata: { sender },
-    //                                     messageStatusCode: 2,
-    //                                     path: 'test'
-    //                                 });
-    // await messageQueue.enqueueRequestMessage( { httpRequestMessage });
-
     // Act
-    const { httpResponseMessage } = await httpMessageHandler.send({ path: '/', headers: { sender }, method: 'POST', data: 'Hello World!' });
+    const { message } = createMessage({ userId, data: 'Hello World!', metadata: { sender, method: 'POST' }, messageStatusCode: 2 });
+    const { httpResponseMessage } = await httpMessageHandler.send({ requestMessage: message });
 
     // Assert
     expect(httpResponseMessage).not.toBeNull();
