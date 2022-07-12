e97b6d1c03b6d84db079c4154cbfb9da5f71c02f
apptesting <marchuanv@sahomeloans.com>
2022-07-13 01:24:13 +0200

    refactor

--- spec/factory/messagebus.factory.spec.js
+++ spec/factory/messagebus.factory.spec.js
@@ -5,7 +5,7 @@ describe('when asking the MessageBus factory to create an instance', function() 
     const {timeout,userId,channelName,host,port,httpMessageHandler,webSocketMessageHandler} = require('C:\\component\\spec\\factory\\messagebus.factory.spec.variables.json');
 
     // Act
-    const {messageBus} = createMessageBus({host,port,userId,channelName});
+    const {messageBus} = createMessageBus({userId,channelName});
     // Assert
     expect(messageBus).not.toBeNull();
   });
--- spec/messagebus/messagebus.spec.js
+++ spec/messagebus/messagebus.spec.js
@@ -2,15 +2,12 @@ describe("when publishing a message", function() {
  it("it should send the same message to all subscribers", async function() {
 
     // Arrange
-    const host = "localhost";
-    const port = 3000;
     const userId = 'joe';
-    const timeout = 5000;
     const channelName = 'messagebustest';
     const expectedData = 'hello from subscription callback';
 
     const { createMessageBus } = require('../../lib/factory/messagebus.factory');
-    const { messageBus, subscription } = createMessageBus({ host, port, userId, timeout, channelName });
+    const { messageBus, subscription } = createMessageBus({ userId, channelName });
 
     subscription.onDataReceived({ callback: ({ host, port, data }) => {
       // Assert
