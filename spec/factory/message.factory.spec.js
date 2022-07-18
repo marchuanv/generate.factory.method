b0f14cb46ac2a3eeb0df8698f5eda1059de253bb
apptesting <marchuanv@sahomeloans.com>
2022-07-18 04:45:31 +0200

    refactor

--- spec/factory/message.factory.spec.js
+++ spec/factory/message.factory.spec.js
@@ -2,10 +2,10 @@ const { createMessage } = require('C:\\component\\lib\\factory\\message.factory.
 describe('when asking the Message factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {senderHost,senderPort,userId,data,token,messageStatusCode,metadata} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
+    const {senderHost,senderPort,userId,data,token,messageStatusCode,metadata,recipientHost,recipientPort} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
 
     // Act
-    const {message} = createMessage({senderHost,senderPort,userId,data,token,metadata,messageStatusCode});
+    const {message} = createMessage({recipientHost,recipientPort,userId,data,senderHost,senderPort,token,metadata,messageStatusCode});
     // Assert
     expect(message).not.toBeNull();
   });
--- spec/factory/messagebus.factory.spec.js
+++ spec/factory/messagebus.factory.spec.js
@@ -2,10 +2,10 @@ const { createMessageBus } = require('C:\\component\\lib\\factory\\messagebus.fa
 describe('when asking the MessageBus factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {userId,channelName,senderHost,senderPort,data,token,messageStatusCode,metadata} = require('C:\\component\\spec\\factory\\messagebus.factory.spec.variables.json');
+    const {userId,channelName,senderHost,senderPort,data,token,messageStatusCode,metadata,recipientHost,recipientPort} = require('C:\\component\\spec\\factory\\messagebus.factory.spec.variables.json');
 
     // Act
-    const {messageBus} = createMessageBus({userId,senderHost,senderPort,data,token,metadata,messageStatusCode,channelName});
+    const {messageBus} = createMessageBus({userId,senderHost,senderPort,recipientHost,recipientPort,data,token,metadata,messageStatusCode,channelName});
     // Assert
     expect(messageBus).not.toBeNull();
   });
