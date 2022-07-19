53d80f6e5756f216b82b22fd348123c7986a1c4f
apptesting <marchuanv@sahomeloans.com>
2022-07-19 02:05:00 +0200

    refactor

--- spec/messagequeue/httpservermessagequeue.spec.js
+++ spec/messagequeue/httpservermessagequeue.spec.js
@@ -2,7 +2,7 @@ const { createHttpServerMessageQueue } = require("../../lib/factory/httpserverme
 const { createHttpClientMessageQueue } = require("../../lib/factory/httpclientmessagequeue.factory");
 const { createMessage } = require("../../lib/factory/message.factory");
 describe("when asking", function() {
-  it("it should", function() {
+  it("it should", async function() {
 
     // Arrange
     const recipientHost = 'localhost';
@@ -13,23 +13,31 @@ describe("when asking", function() {
     const messageStatusCode = 2;
     const metadata = {};
     const data = 'Hello World';
-
-    const { httpServerMessageQueue } = createHttpServerMessageQueue({ 
-      recipientHost, recipientPort, userId, 
-      senderHost, senderPort
-    });
-
-    const { httpClientMessageQueue } = createHttpClientMessageQueue({
-      recipientHost, recipientPort, userId, 
-      senderHost, senderPort
-    });
+    const token = null;
+    let _httpClientMessageQueue01 = null;
+    let _httpClientMessageQueue02 = null;
+    {
+      ({ httpClientMessageQueue: _httpClientMessageQueue01 } = createHttpClientMessageQueue({
+        recipientHost, recipientPort, userId, 
+        senderHost, senderPort
+      }));
+    }
+    {
+      ({ httpClientMessageQueue: _httpClientMessageQueue02 } = createHttpClientMessageQueue({
+        recipientHost, recipientPort, userId, 
+        senderHost, senderPort
+      }));
+    }
 
     // Act
-    httpClientMessageQueue.enqueueHttpRequest(createMessage({
-      recipientHost, recipientPort, userId, data,
-      senderHost, senderPort, token, metadata, messageStatusCode
-    }));
+    {
+      _httpClientMessageQueue01.sync({ httpClientMessageQueue:  _httpClientMessageQueue02 });
+      const { message } = createMessage({ recipientHost, recipientPort, userId, data, senderHost, senderPort, token, metadata, messageStatusCode });
+      _httpClientMessageQueue01.enqueueHttpRequestMessage({ requestMessage: message });
+    }
 
     // Assert
+    const { httpRequestMessage } = await _httpClientMessageQueue02.dequeueHttpRequestMessage();
+    expect(httpRequestMessage).not.toBeNull();
   });
 });
