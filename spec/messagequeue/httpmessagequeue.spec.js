7886eb7a33fcf01aeaae3d1265bd5a7d1c2d01e2
apptesting <marchuanv@sahomeloans.com>
2022-07-19 03:02:48 +0200

    refactor

--- spec/messagequeue/httpmessagequeue.spec.js
+++ spec/messagequeue/httpmessagequeue.spec.js
@@ -39,7 +39,51 @@ describe("when queuing http messages", function() {
     const { httpRequestMessage } = await _httpClientMessageQueue02.dequeueHttpRequestMessage();
     expect(httpRequestMessage).not.toBeNull();
   });
-  it("it should dequeue http response messages without error", async function() {
+  it("it should dequeue server http request messages without error", async function() {
+
+    // Arrange
+    const recipientHost = 'localhost';
+    const recipientPort = 3000;
+    const senderHost = 'localhost';
+    const senderPort = 3000;
+    const userId = 'joe';
+    const messageStatusCode = 2;
+    const metadata = {};
+    const data = 'Hello World';
+    const token = null;
+    let _httpServerMessageQueue01 = null;
+    let _httpServerMessageQueue02 = null;
+    const { createHttpServerMessageQueue } = require("../../lib/factory/httpservermessagequeue.factory");
+    const { createMessage } = require("../../lib/factory/message.factory");
+    {
+      ({ httpServerMessageQueue: _httpServerMessageQueue01 } = createHttpServerMessageQueue({
+        recipientHost, recipientPort, userId, 
+        senderHost, senderPort
+      }));
+    }
+    {
+      ({ httpServerMessageQueue: _httpServerMessageQueue02 } = createHttpServerMessageQueue({
+        recipientHost, recipientPort, userId, 
+        senderHost, senderPort
+      }));
+    }
+
+    // Act
+    {
+      _httpServerMessageQueue01.sync({ httpServerMessageQueue:  _httpServerMessageQueue02 });
+      _httpServerMessageQueue01.enqueueHttpRequest({ httpRequest: {
+        body: 'Hello World',
+        headers: {},
+        path: "/test",
+        method: "POST"
+      }});
+    }
+
+    // Assert
+    const { httpRequestMessage } = await _httpServerMessageQueue02.dequeueHttpRequestMessage();
+    expect(httpRequestMessage).not.toBeNull();
+  });
+  it("it should dequeue server http response messages without error", async function() {
 
     // Arrange
     const recipientHost = 'localhost';
@@ -76,7 +120,7 @@ describe("when queuing http messages", function() {
     }
 
     // Assert
-    const { httpRequestMessage } = await _httpServerMessageQueue02.dequeueHttpResponseMessage();
-    expect(httpRequestMessage).not.toBeNull();
+    const { httpResponseMessage } = await _httpServerMessageQueue02.dequeueHttpResponseMessage();
+    expect(httpResponseMessage).not.toBeNull();
   });
 });
