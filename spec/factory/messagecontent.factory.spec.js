a96862d1efd39ebc4239a2e0e7e19c8c3306a877
apptesting <marchuanv@sahomeloans.com>
2022-07-26 19:04:38 +0200

    refactor

--- spec/factory/messagecontent.factory.spec.js
+++ spec/factory/messagecontent.factory.spec.js
@@ -2,7 +2,7 @@ const { createMessageContent } = require('C:\\component\\lib\\factory\\messageco
 describe('when asking the MessageContent factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {data,messageContentSecurity,userId,metadata} = require('C:\\component\\spec\\factory\\messagecontent.factory.spec.variables.json');
+    const {data,userId,metadata} = require('C:\\component\\spec\\factory\\messagecontent.factory.spec.variables.json');
 
     // Act
     const {messageContent} = createMessageContent({data,userId,metadata});
