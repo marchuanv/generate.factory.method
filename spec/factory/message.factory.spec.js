d220e756246d28c10d91f73acd51cf68d3df1b00
apptesting <marchuanv@sahomeloans.com>
2022-07-27 11:52:06 +0200

    refactor

--- spec/factory/message.factory.spec.js
+++ spec/factory/message.factory.spec.js
@@ -2,7 +2,7 @@ const { createMessage } = require('C:\\component\\lib\\factory\\message.factory.
 describe('when asking the Message factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {senderHost,senderPort,userId,data,messageStatusCode,metadata,recipientHost,recipientPort,Id,recipientAddress,messageContent,messageContentMetadata,messageMetadata,messageStatus,senderAddress} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
+    const {senderHost,senderPort,userId,data,messageStatusCode,metadata,recipientHost,recipientPort,Id} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
 
     // Act
     const {message} = createMessage({recipientHost,recipientPort,Id,data,userId,metadata,messageStatusCode,senderHost,senderPort});
