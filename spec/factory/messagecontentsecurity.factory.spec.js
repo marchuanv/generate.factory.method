df712eb367f6acee6fc42580c455259599efb95f
apptesting <marchuanv@sahomeloans.com>
2022-08-04 15:15:38 +0200

    refactor

--- spec/factory/messagecontentsecurity.factory.spec.js
+++ spec/factory/messagecontentsecurity.factory.spec.js
@@ -5,7 +5,7 @@ describe('when asking the MessageContentSecurity factory to create an instance',
     const {data,metadata,recipientHost,recipientPort,senderHost,senderPort} = require('C:\\component\\spec\\factory\\messagecontentsecurity.factory.spec.variables.json');
 
     // Act
-    const {messageContentSecurity} = createMessageContentSecurity({data,recipientHost,recipientPort,metadata,senderHost,senderPort});
+    const {messageContentSecurity} = createMessageContentSecurity({recipientHost,recipientPort,metadata,senderHost,senderPort});
     // Assert
     expect(messageContentSecurity).not.toBeNull();
   });
