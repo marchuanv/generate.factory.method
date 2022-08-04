ab465c8f87cc2ef496df2a3231e2909e247a5364
apptesting <marchuanv@sahomeloans.com>
2022-08-04 15:47:51 +0200

    refactor

---
+++ spec/factory/sharedusersessions.factory.spec.js
@@ -0,0 +1,12 @@
+const { createSharedUserSessions } = require('C:\\component\\lib\\factory\\sharedusersessions.factory.js');
+describe('when asking the SharedUserSessions factory to create an instance', function() {
+  it("it should succeed without any errors", function() {
+    // Arrange
+    const {} = require('C:\\component\\spec\\factory\\sharedusersessions.factory.spec.variables.json');
+
+    // Act
+    const {sharedUserSessions} = createSharedUserSessions({});
+    // Assert
+    expect(sharedUserSessions).not.toBeNull();
+  });
+});
