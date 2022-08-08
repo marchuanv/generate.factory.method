201bd730a98743e8107a09d7a6fdcfee8ae04c1c
apptesting <marchuanv@sahomeloans.com>
2022-08-08 16:31:26 +0200

    fixing tests

--- spec/factory/message.factory.spec.js
+++ spec/factory/message.factory.spec.js
@@ -2,7 +2,7 @@ const { createMessage } = require('C:\\component\\lib\\factory\\message.factory.
 describe('when asking the Message factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {senderHost,senderPort,data,messageStatusCode,metadata,recipientHost,recipientPort,Id} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
+    const {Id,recipientHost,recipientPort,senderHost,senderPort,data,messageStatusCode,metadata} = require('C:\\component\\spec\\factory\\message.factory.spec.variables.json');
 
     // Act
     const {message} = createMessage({messageStatusCode,Id,data,recipientHost,recipientPort,metadata,senderHost,senderPort});
--- spec/factory/messagebus.factory.spec.js
+++ spec/factory/messagebus.factory.spec.js
@@ -2,10 +2,10 @@ const { createMessageBus } = require('C:\\component\\lib\\factory\\messagebus.fa
 describe('when asking the MessageBus factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {senderHost,senderPort,recipientHost,recipientPort,messageQueueTypeCode,channel,remoteBase64RSAPublicKey,userId} = require('C:\\component\\spec\\factory\\messagebus.factory.spec.variables.json');
+    const {senderHost,senderPort,recipientHost,recipientPort,messageQueueTypeCode,channel,userId,secret} = require('C:\\component\\spec\\factory\\messagebus.factory.spec.variables.json');
 
     // Act
-    const {messageBus} = createMessageBus({userId,messageQueueTypeCode,senderHost,senderPort,recipientHost,recipientPort,channel});
+    const {messageBus} = createMessageBus({secret,messageQueueTypeCode,senderHost,senderPort,recipientHost,recipientPort,channel,userId});
     // Assert
     expect(messageBus).not.toBeNull();
   });
