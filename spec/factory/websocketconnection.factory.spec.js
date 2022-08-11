2623e09b38880a6ebc7380ee274b9e90287f5116
apptesting <marchuanv@sahomeloans.com>
2022-08-11 11:09:14 +0200

    fixing tests

--- spec/factory/websocketconnection.factory.spec.js
+++ spec/factory/websocketconnection.factory.spec.js
@@ -2,7 +2,7 @@ const { createWebSocketConnection } = require('C:\\component\\lib\\factory\\webs
 describe('when asking the WebSocketConnection factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {timeout,host,port,hostPort,hostAddress} = require('C:\\component\\spec\\factory\\websocketconnection.factory.spec.variables.json');
+    const {timeout,hostAddress} = require('C:\\component\\spec\\factory\\websocketconnection.factory.spec.variables.json');
 
     // Act
     const {webSocketConnection} = createWebSocketConnection({timeout,hostAddress});
