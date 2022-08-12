59e668081f0e84265a6d9e718ea91a9d570cee57
apptesting <marchuanv@sahomeloans.com>
2022-08-12 14:10:04 +0200

    fixing tests

--- spec/messagebus/clientmessagebus.spec.js
+++ spec/messagebus/clientmessagebus.spec.js
@@ -23,38 +23,46 @@ fdescribe("when asking the client message bus publish and subscribe to messages"
     const timeout = 15000;
     const contextId = 'messagebustests';
     const metadata = {};
-    let expectedDecryptedClientText;
-    let expectedDecryptedServerText;
-    let assertCallback = null;
-
+    let expectedDecryptedClientText = 'Hello From Client';
+    let expectedDecryptedServerText = 'Hello From Server';
+    let requestMessage = null;
     const { createMessage } = require('../../lib/factory/message.factory');
-    const { createHttpResponseMessage } = require('../../lib/factory/httpresponsemessage.factory');
+
+    { 
+      //Simulate a Server
+      const { createServerMessageBus } = require('../../lib/factory/servermessagebus.factory.js');
+      const { serverMessageBus } = createServerMessageBus({ timeout, contextId, senderHost, senderPort });
+      serverMessageBus.initialise();
+      const { message } = createMessage({ messageStatusCode: 0, Id: null, data: expectedDecryptedServerText, recipientHost, recipientPort, metadata, token, senderHost, senderPort });
+      serverMessageBus.publishMessage({ message });
+      serverMessageBus.subscribeToMessages({ callback: ({ message }) => {
+        requestMessage = message;
+      }});
+    }
+
     const { createClientMessageBus } = require('../../lib/factory/clientmessagebus.factory.js');
-    const { createHttpServerMessageBus } = require('../../lib/factory/httpservermessagebus.factory.js');
     const { clientMessageBus } = createClientMessageBus({ timeout, contextId, senderHost, senderPort });
-    const { httpServerMessageBus } = createHttpServerMessageBus({ timeout, contextId, senderHost, senderPort });
 
-    {
-      const { httpResponseMessage } = createHttpResponseMessage({ messageStatusCode: 0, Id: null, data: 'Hello World From Server', recipientHost, recipientPort, metadata, token, senderHost, senderPort });
-      httpServerMessageBus.publishHttpResponseMessage({ httpResponseMessage });
-    }
-
-    clientMessageBus.subscribeToMessages({ callback: ({ message }) => {
-      assertCallback({ responseMessage: message });
-    }});
-   
     // Act
-    httpServerMessageBus.initialise();
-    const { message } = createMessage({ messageStatusCode: 2, Id: null, data: 'Hello From Client', recipientHost, recipientPort, metadata: { path }, token, senderHost, senderPort });
-    {
-      const { text } = message.getDecryptedContent();
-      expectedDecryptedClientText = text;
-    }
-    clientMessageBus.publishMessage({ message });
+    clientMessageBus.publishMessage(createMessage({ 
+      messageStatusCode: 2, Id: null, data: expectedDecryptedClientText,
+      recipientHost, recipientPort, metadata: { path }, token, senderHost, senderPort 
+    }));
 
     // Assert
-    assertCallback = ({ responseMessage }) => {
-      expect(messageBus.isOpen()).toBeFalsy();
+    clientMessageBus.subscribeToMessages({ callback: ({ message }) => {
+      const responseMessage = message;
+      expect(responseMessage).not.toBeUndefined();
+      expect(responseMessage).not.toBeNull();
+      {
+        const { text } = responseMessage.getDecryptedContent();
+        expect(text).toEqual(expectedDecryptedServerText);
+      }
+      {
+        const { code } = responseMessage.getMessageStatus();
+        expect(code).toEqual(0); //success
+      }
+
       expect(requestMessage).not.toBeUndefined();
       expect(requestMessage).not.toBeNull();
       {
@@ -70,18 +78,8 @@ fdescribe("when asking the client message bus publish and subscribe to messages"
         expect(senderHost).toEqual('localhost');
         expect(senderPort).toEqual(3000);
       }
-      expect(responseMessage).not.toBeUndefined();
-      expect(responseMessage).not.toBeNull();
-      {
-        const { text } = responseMessage.getDecryptedContent();
-        expect(text).toEqual(expectedDecryptedServerText);
-      }
-      {
-        const { code } = responseMessage.getMessageStatus();
-        expect(code).toEqual(0); //success
-      }
+
       done();
-    }
-  
+    }});
   });
 });
