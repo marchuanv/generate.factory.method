b536076df514b22fe08b0ce498d4a0d26ce3fccb
marchuanv <marchuanv@gmail.com>
2022-08-14 02:00:25 +0200

    fix

--- spec/component/component.spec.js
+++ spec/component/component.spec.js
@@ -6,33 +6,39 @@ fdescribe("when initialising a component given a userid and secret", function() 
     
   });
 
- it("it should creste a secure messagebus", async function() {
+ it("it should creste a secure messagebus", function(done) {
   
-    // Arrange
-    {
-      // Server Component
-      const { createComponent } = require('../../lib/factory/component.factory.js');
-      const { component } = createComponent({ packageJson: {
-        userId: 'componenttest',
-        recipientAddress: { recipientHost: 'localhost', recipientPort: 3000 },
-        senderAddress: { senderHost: 'localhost', senderPort: 2000 },
-        isServerComponent: true
-      }});
-      await component.initialise({ secret: 'secret1234' });
-    }
-    {
-      // Client Component
-      const { createComponent } = require('../../lib/factory/component.factory.js');
-      const { component } = createComponent({ packageJson: {
-        userId: 'componenttest',
-        recipientAddress: { recipientHost: 'localhost', recipientPort: 2000 },
-        senderAddress: { senderHost: 'localhost', senderPort: 3000 },
-        isServerComponent: false
-      }});
-      await component.initialise({ secret: 'secret1234' });
-    }
-
+  // Arrange
   // Act
+  {
+    // Server Component
+    const { createComponent } = require('../../lib/factory/component.factory.js');
+    const { component } = createComponent({ packageJson: {
+      userId: 'componenttest',
+      senderAddress: { senderHost: 'localhost', senderPort: 2000 },
+      isServerComponent: true
+    }});
+    component.initialise({ secret: 'secret1234' }).then(() => {
+      component.receive({ callback: ({ message }) => {
+        component.send({ object: { text: 'Hello From Server' } });
+      }});
+    });
+  }
+  {
+    // Client Component
+    const { createComponent } = require('../../lib/factory/component.factory.js');
+    const { component } = createComponent({ packageJson: {
+      userId: 'componenttest',
+      recipientAddress: { recipientHost: 'localhost', recipientPort: 2000 },
+      isServerComponent: false
+    }});
+    component.initialise({ secret: 'secret1234' }).then(() => {
+      component.send({ object: { text: 'Hello From Client' } });
+      component.receive({ callback: ({ message }) => {
+        done();
+      }});
+    });
+  }
   // Assert
  });
 });
