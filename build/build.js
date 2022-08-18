511d24d2e76a0960f61e1d9c6bbc5aa2a71faff7
apptesting <marchuanv@sahomeloans.com>
2022-08-18 09:48:49 +0200

    removed shared function naming and created config to indicate singletons

--- build/build.js
+++ build/build.js
@@ -37,7 +37,7 @@ function getDependencyTree(typeInfo, pass = 'firstpass', types = []) {
                 const sc = require(scriptPath);
                 const key = Object.keys(sc)[0];
                 const type = sc[key];
-                const singleton = containerConfig.find(cConf => cConf.typeName === type.name && cConf.singleton) || false;
+                const singleton = containerConfig.find(cConf => cConf.typeName.toLowerCase() === key.toLowerCase() && cConf.singleton) ? true : false;
                 const parameters = utils.getFunctionParams(type) || [];
                 const children = parameters.map(param => utils.getJSONObject(typeInfoTemplate
                     .replace(/\[TypeName\]/g, param.name)
