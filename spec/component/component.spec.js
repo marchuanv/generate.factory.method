4d23ee5001c21b55f2e58bc96aec68d8f8ce9329
apptesting <marchuanv@sahomeloans.com>
2022-08-31 15:12:43 +0200

    refactor

--- spec/component/component.spec.js
+++ spec/component/component.spec.js
@@ -1,8 +1,8 @@
-describe("when initialising a component given a client and server messagebus", function() {
+fdescribe("when initialising a component given a client and server messagebus", function() {
 
-  let token = null;
   let clientScopeId = "clientcomponenttest";
   let serverScopeId = "servercomponenttest";
+  const { createComponent } = require('../../lib/factory/component.factory.js');
 
   beforeAll(() => {
     
@@ -11,40 +11,44 @@ describe("when initialising a component given a client and server messagebus", f
  it("it should provide the capability for sending and receiving messages.", function(done) {
   
   // Arrange
-  const { createComponent } = require('../../lib/factory/component.factory.js');
-
-  // Act
+  let clientComponent;
+  let serverComponent;
   {
-    // Server Component
-    const { component } = createComponent({ scopeId: clientScopeId, packageJson: {
-      userId: 'componenttest',
-      senderHost: 'localhost', senderPort: 3000,
-      recipientHost: 'localhost', recipientPort: 2000,
-      timeout: 15000,
-      isServerComponent: true
-    }});
-    component.initialise({ secret: 'secret1234' }).then(() => {
-      component.receive({ callback: ({ message }) => {
-        component.send({ object: { text: 'Hello From Server' } });
-      }});
-    });
-  }
-  {
-    // Client Component
     const { component } = createComponent({ scopeId: serverScopeId, packageJson: {
       userId: 'componenttest',
       senderHost: 'localhost', senderPort: 2000,
       recipientHost: 'localhost', recipientPort: 3000,
       timeout: 15000,
+      isServerComponent: true
+    }});
+    serverComponent = component;
+  }
+  {
+    const { component } = createComponent({ scopeId: clientScopeId, packageJson: {
+      userId: 'componenttest',
+      senderHost: 'localhost', senderPort: 3000,
+      recipientHost: 'localhost', recipientPort: 2000,
+      timeout: 15000,
       isServerComponent: false
     }});
-    component.initialise({ secret: 'secret1234' }).then(() => {
-      component.send({ object: { text: 'Hello From Client' } });
-      component.receive({ callback: ({ message }) => {
-        setTimeout(done,1500);
-      }});
-    });
+    clientComponent = component;
   }
+
+  // Act
+  clientComponent.initialise({ secret: 'secret1234' }).then(() => {
+    clientComponent.send('Hello From Client');
+    clientComponent.receive({ callback: () => {
+      setTimeout(done,1500);
+    }});
+  });
+  serverComponent.initialise({ secret: 'secret1234' }).then(() => {
+    serverComponent.receive({ callback: () => {
+      serverComponent.send('Hello From Server');
+    }});
+  });
+
   // Assert
+
+
  });
 });
