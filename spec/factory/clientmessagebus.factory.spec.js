54b8c43a77413d56b884271e4caf4edea50f1414
apptesting <marchuanv@sahomeloans.com>
2022-08-31 14:14:05 +0200

    refactor

--- spec/factory/clientmessagebus.factory.spec.js
+++ spec/factory/clientmessagebus.factory.spec.js
@@ -2,7 +2,7 @@ const { createClientMessageBus } = require('C:\\component\\lib\\factory\\clientm
 describe('when asking the ClientMessageBus factory to create an instance', function() {
   it("it should succeed without any errors", function() {
     // Arrange
-    const {scopeId,httpClientRequestMessageBus,httpClientResponseMessageBus} = require('C:\\component\\spec\\factory\\clientmessagebus.factory.spec.variables.json');
+    const {scopeId} = require('C:\\component\\spec\\factory\\clientmessagebus.factory.spec.variables.json');
 
     // Act
     const {clientMessageBus} = createClientMessageBus({scopeId});
