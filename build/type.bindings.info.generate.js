54eb63498749ea39ce64f635757df4d01b2de0d3
marchuanv <marchuanv@gmail.com>
2022-10-03 22:05:17 +0200

    fix

--- build/type.bindings.info.generate.js
+++ build/type.bindings.info.generate.js
@@ -4,24 +4,28 @@ const utils = require('utils');
 const typesInfo = require(path.join(__dirname, 'types.info.json'));
 const typeBindingInfoTemplate = readFileSync(path.join(__dirname, 'templates', 'type.binding.info.template'),'utf8');
 const typeBindingsInfoPath = path.join(__dirname, 'type.bindings.info.json');
-const typeBindingsInfo = [];
-const bindingsInfo = require(path.join(__dirname, 'bindings.info.json'));
+const typeBindingsInfo = require(typeBindingsInfoPath);
 
 for(const typeName of Object.keys(typesInfo)) {
     const typeInfo = typesInfo[typeName];
     const { children } = typeInfo;
-    for(const bindingName of Object.keys(bindingsInfo)) {
-        const { isSingleton } = bindingsInfo[bindingName];
-        const ctorParameterInfo = [];
-        children.forEach((child) => {
-            const name = child.variableName;
-            const typeName =  typesInfo[child.typeName] ? child.typeName : null;
-            ctorParameterInfo.push({ name, typeName });
-        });
+    const ctorParameterInfo = [];
+    children.forEach((child) => {
+        const name = child.variableName;
+        const typeName =  typesInfo[child.typeName] ? child.typeName : null;
+        ctorParameterInfo.push({ name, typeName });
+    });
+    const foundTypeBinding = typeBindingsInfo.find(bind => bind.typeName === typeName);
+    if (foundTypeBinding) {
+        foundTypeBinding.ctorParameterInfo = ctorParameterInfo;
+        if (foundTypeBinding.isSingleton === null || foundTypeBinding.isSingleton === undefined) {
+            foundTypeBinding.isSingleton = false;
+        }
+    } else {
         typeBindingsInfo.push(utils.getJSONObject(typeBindingInfoTemplate
             .replace(/\[TypeName\]/g, typeName)
-            .replace(/\[BindingName\]/g, bindingName)
-            .replace(/\[IsSingleton\]/g, isSingleton)
+            .replace(/\[BindingNames\]/g, '[]')
+            .replace(/\[IsSingleton\]/g, false)
             .replace(/\[CtorParameterInfo\]/g, utils.getJSONString(ctorParameterInfo))
             .replace(/\[DependantBindings\]/g, utils.getJSONString([]))));
     }
