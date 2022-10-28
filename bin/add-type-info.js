0ef853db53a9eaaf5f12ca567f72cc7ece029347
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2022-10-28 08:17:13 +0200

    refactor

--- bin/add-type-info.js
+++ bin/add-type-info.js
@@ -1,62 +1,10 @@
 #!/usr/bin/env node
-const path = require('path');
 const typeInfo = require('../lib/type.info');
-const factoryContainer = require('../lib/factory.container');
 const { existsSync } = require('fs');
-const { readdirSync, writeFileSync, statSync, readFileSync } = require('fs');
-const factoryTemplate = readFileSync(path.join(__dirname, '../lib', 'templates', 'factory.template'),'utf8');
-
-function walk(dir) {
-    let results = [];
-    const list = readdirSync(dir);
-    list.forEach(function(file) {
-        const _file = path.join(dir, '/', file);
-        const stat = statSync(_file);
-        if (stat && stat.isDirectory()) { 
-            /* Recurse into a subdirectory */
-            results = results.concat(walk(_file));
-        } else { 
-            /* Is a file */
-            results.push(_file);
-        }
-    });
-    return results;
-}
-
-const args = {
-    libGuid: null,
-    scriptPath: null,
-    prototypeScriptPath: null,
-    isSingleton: null,
-    isContextSingleton: null,
-    scriptOutputDirPath: null,
-    contextName: null,
-    defaultContextName: null
-}
-
-process.argv.forEach( (param, index) => {
-    const name = param.replace('--','');
-    if (args[name] !== undefined) {
-        let value = process.argv[index + 1];
-        if (value.toLowerCase().indexOf('true') > -1) {
-            value = true;
-        } else if (value.toLowerCase().indexOf('false') > -1) {
-            value = false;
-        }
-        if (typeof value === 'string' && (value.indexOf('/') > -1 || value.indexOf('\\') > -1 ) ) {
-            value = path.resolve(value);
-        }
-        args[name] = value;
-    }
-});
-
-const { scriptPath, prototypeScriptPath, isSingleton, isContextSingleton, scriptOutputDirPath, contextName, defaultContextName } = args;
+const { scriptPath, isSingleton, scriptOutputDirPath } = require('./map.arguments');
 
 if (!existsSync(scriptPath)) {
     throw new Error(`${scriptPath} script does not exist.`);
-}
-if (!existsSync(prototypeScriptPath)) {
-    throw new Error(`${prototypeScriptPath} script does not exist.`);
 }
 if (!existsSync(scriptOutputDirPath)){
     throw new Error(`${scriptOutputDirPath} script does not exist.`);
@@ -67,25 +15,3 @@ const key = Object.keys(script)[0];
 const type = script[key];
 
 typeInfo.add({ type, isSingleton, typeInfoOutputDirPath:  scriptOutputDirPath });
-const _typeInfo = typeInfo.get({ type, typeInfoOutputDirPath: scriptOutputDirPath });
-
-const ctorArgumentsWithContextNames = _typeInfo.childInfoArray.map(ci => ci.variableName);
-ctorArgumentNames = ctorArgumentsWithContextNames.concat(['contextName']);
-
-factoryContainer.generate({ 
-    typeInfo: _typeInfo,
-    scriptPath,
-    isContextSingleton,
-    contextName,
-    defaultContextName,
-    jsonOutputDirPath: scriptOutputDirPath
-});
-
-const factoryScriptPath = path.join(scriptOutputDirPath, `${_typeInfo.typeName.toLowerCase()}.factory.js`);
-const containerFilePaths = walk(scriptOutputDirPath).filter(p => p.indexOf(_typeInfo.typeName.toLowerCase()) > -1 ).map(p => p.replace(/\\/g,'//'));
-const factoryJs = factoryTemplate
-    .replace(/\[ContextFilePaths\]/g, JSON.stringify(containerFilePaths))
-    .replace(/\[TypeName\]/g, _typeInfo.typeName)
-    .replace(/\[PrimitiveArgs\]/g, ctorArgumentNames.join(','))
-    .replace(/\[PrimitiveArgsWithContextName\]/g, ctorArgumentsWithContextNames.join(','));
-writeFileSync(factoryScriptPath, factoryJs, 'utf8');
